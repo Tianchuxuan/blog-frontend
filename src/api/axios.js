0f38bbe1bef7e2279078f34bd6122b2ef327f25c
Tianchuxuan <yesterday6333@qq.com>
2025-11-21 12:13:57 +0800

    更新后端API链接

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://blog-api-bldx.onrender.com/',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://blog-api-bldx.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
